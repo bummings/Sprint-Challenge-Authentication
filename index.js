482a7c7291ecdc6aa1cf81bf1e9759383e86f24e
bummings <eabummings@gmail.com>
2018-11-30 11:39:03 -0500

    added dependencies, server is running

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { server } = require('./server.js');
+
+const db = require('./database/dbConfig.js');
+
+// test root route
+server.get('/', (req, res) => {
+  res.send('server is runnin on 3300');
+});
 
 const port = process.env.PORT || 3300;
 server.listen(port, () => {
