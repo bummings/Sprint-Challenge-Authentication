38d638dff280000cb6fada86bd0a6cb07d378439
bummings <eabummings@gmail.com>
2018-11-30 12:28:11 -0500

    moved around some dependencies, register route working

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,7 @@
 const axios = require('axios');
+const db = require('../database/dbConfig.js');
+const { server } = require('../server.js');
+const bcrypt = require('bcryptjs');
 
 const { authenticate } = require('./middlewares');
 
@@ -6,10 +9,25 @@ module.exports = server => {
   server.post('/api/register', register);
   server.post('/api/login', login);
   server.get('/api/jokes', authenticate, getJokes);
+  server.get('/', rootRoute);
 };
 
+// testing root route
+function rootRoute(req, res) {
+  res.send('server is runnin on 3300');
+}
+
 function register(req, res) {
-  // implement user registration
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password);
+  creds.password = hash;
+
+  db('users')
+    .insert(creds)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => json(err));
 }
 
 function login(req, res) {
@@ -18,9 +36,7 @@ function login(req, res) {
 
 function getJokes(req, res) {
   axios
-    .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
-    )
+    .get('https://safe-falls-22549.herokuapp.com/random_ten')
     .then(response => {
       res.status(200).json(response.data);
     })
--- index.js
+++ index.js
@@ -5,11 +5,6 @@ const { server } = require('./server.js');
 
 const db = require('./database/dbConfig.js');
 
-// test root route
-server.get('/', (req, res) => {
-  res.send('server is runnin on 3300');
-});
-
 const port = process.env.PORT || 3300;
 server.listen(port, () => {
   console.log(`\n=== Server listening on port ${port}\n`);
